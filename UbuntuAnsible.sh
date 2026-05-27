93a9b18cb75a9636c731b5d584e0a0c5150d077d
Guillem Hernandez Sola <guillem@agile611.com>
2026-05-27 11:41:50 +0200

    added all

--- UbuntuAnsible.sh
+++ UbuntuAnsible.sh
@@ -1,6 +1,9 @@
 # Script to install Ansible on a Ubuntu system
 apt-get update
-# Add Ansible repository and install Ansible
+# Install required packages
+apt install software-properties-common -y   
+# Add Ansible PPA and install Ansible
+apt-add-repository ppa:ansible/ansible
 apt-get install ansible net-tools -y
 # Add vagrant user to sudoers
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/vagrant
